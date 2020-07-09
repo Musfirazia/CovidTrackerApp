31cc5179ddd86da7bdfe6d3a1f03f944d9b9e126
Musfirazia <42489563+Musfirazia@users.noreply.github.com>
2020-07-09 23:49:47 +0500

    Covid-19 Tracker App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Navbar from './Components/Navbar.js'
+import InfoBar from './Components/InfoBar.js'
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+    margin:'15px',
+  },
+ 
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.root}>
+          <Navbar/>
+     <InfoBar/>
+    
+    </div>
     </div>
   );
 }
